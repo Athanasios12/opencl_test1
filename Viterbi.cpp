c30178d71c37a1ebab79cedfe63a6f393c5dc653
Radoslaw Rajczyk <radoslaw.rajczyk@synapse.com>
2017-06-09 15:09:44 +0200

    minor fixes

--- Viterbi.cpp
+++ Viterbi.cpp
@@ -8,7 +8,9 @@ Viterbi::Viterbi(const cl_command_queue & command_queue, const cl_context & cont
 	m_img(NULL),
 	m_command_queue(command_queue),
 	m_context(context),
-	m_device_id(device_id)
+	m_device_id(device_id),
+	m_set_hybrid_rate(false),
+	m_hybrid_rate(std::pair<double, double>(0.5, 0.5))
 { 
 	m_initalized = loadAndBuildKernel();
 }
@@ -25,22 +27,21 @@ Viterbi::~Viterbi()
 bool Viterbi::loadAndBuildKernel()
 {
 	int err = 0;
-	//----------initalization, do it in contsructor - needs to be executed only once
 	std::string source_str;
-	// Load the source code containing the kernel*/
+	// Load the source code containing the kernel
 	size_t source_size = readKernelFile(source_str, VITERBI_KERNEL_FILE);
 	if (source_size == 0)
 	{
-		return 1;
+		return false;
 	}
 	char *source_str_ptr = &source_str[0];
 	 m_program = clCreateProgramWithSource(m_context, 1, (const char **)&source_str_ptr,
 		(const size_t *)&source_size, &err);
 	if (CL_SUCCESS != err)
 	{
-		return err;
+		return false;
 	}
-	// Build Kernel Program */
+	// Build Kernel Program
 	err = clBuildProgram(m_program, 1, &m_device_id, NULL, NULL, NULL);
 	if (err == CL_BUILD_PROGRAM_FAILURE) {
 		// Determine the size of the log
@@ -57,12 +58,10 @@ bool Viterbi::loadAndBuildKernel()
 		printf("%s\n", log);
 		free(log);
 	}
-	// Create OpenCL Kernel */
+	// Create OpenCL Kernel
 	m_viterbiKernel = clCreateKernel(m_program, VITERBI_COLS_FUNCTION, &err);
-	//----------initalization, do it in contsructor - needs to be executed only once
 	return err == CL_SUCCESS;
 }
-
 
 size_t Viterbi::readKernelFile(std::string &source_str, const std::string &fileName)
 {
@@ -83,8 +82,6 @@ size_t Viterbi::readKernelFile(std::string &source_str, const std::string &fileN
 	}
 	return length;
 }
-
-
 
 void Viterbi::fixGlobalSize(size_t &global_size, const size_t &local_size)
 {
@@ -218,7 +215,6 @@ int Viterbi::viterbiLineOpenCL_rows(unsigned int *line_x, int g_low, int g_high)
 	return err;
 }
 
-//template <class pix_type>
 int Viterbi::viterbiLineDetect(std::vector<unsigned int> &line_x, int g_low, int g_high)
 {
 	if (m_img == 0 && m_img_height > 0 && m_img_width > 0)
@@ -231,7 +227,6 @@ int Viterbi::viterbiLineDetect(std::vector<unsigned int> &line_x, int g_low, int
 
 	uint32_t P_max = 0;
 	uint32_t x_max = 0;
-	std::vector<uint32_t> x_cord(m_img_width, 0);
 	uint32_t max_val = 0;
 	size_t i = 0;
 	unsigned char pixel_value = 0;
@@ -278,13 +273,13 @@ int Viterbi::viterbiLineDetect(std::vector<unsigned int> &line_x, int g_low, int
 			}
 		}
 		//backwards phase - retrace the path
-		x_cord[(m_img_width - 1)] = x_max;
+		uint32_t x_n = x_max;
 		for (size_t n = (m_img_width - 1); n > i; n--)
 		{
-			x_cord[n - 1] = x_cord[n] + L[(x_cord[n] * m_img_width) + (n - 1)];
+			x_n = x_n + L[(x_n * m_img_width) + (n - 1)];
 		}
 		// save only last pixel position
-		line_x[i] = x_cord[i];
+		line_x[i] = x_n;
 		P_max = 0;
 		x_max = 0;
 		++i;
@@ -299,22 +294,21 @@ int Viterbi::viterbiLineOpenCL_cols(unsigned int *line_x, int g_low, int g_high)
 	if (!m_initalized || m_img == NULL)
 	{
 		cout << "Kernel not initialized" << endl;
-		return CL_FALSE;
+		return err;
 	}
 	size_t img_size = (m_img_height * m_img_width);
-	size_t global_size = m_img_width; // maybe make it later so it can be divided by Prefered opencl device multiple
+	size_t global_size = m_img_width; 
 
-									//check available memory
-	long long dev_memory = 0;
-	err = clGetDeviceInfo(m_device_id, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(long long), &dev_memory, NULL);
+	//check available memory
+	cl_ulong dev_memory = 0;
+	err = clGetDeviceInfo(m_device_id, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(cl_ulong), &dev_memory, NULL);
 	cl_ulong max_alloc = 0;
-	err = clGetDeviceInfo(m_device_id, CL_DEVICE_MAX_MEM_ALLOC_SIZE, sizeof(long long), &max_alloc, NULL);
+	err = clGetDeviceInfo(m_device_id, CL_DEVICE_MAX_MEM_ALLOC_SIZE, sizeof(cl_ulong), &max_alloc, NULL);
 	int dev_mem = static_cast<int>(double(dev_memory) / double(1024 * 1024));//MB
 	int max_buff_size = static_cast<int>(double(max_alloc) / double(1024 * 1024));
 	int tot_mem = static_cast<int>(double((img_size * global_size * sizeof(float)) +
-		(2 * m_img_height * m_img_width * sizeof(float)) +
+		(2 * m_img_height * global_size * sizeof(float)) +
 		(2 * m_img_width * sizeof(int)) + (img_size * sizeof(unsigned char))) / double(1024 * 1024));
-	//later delete prints for speed
 #ifdef _DEBUG
 	printf("\nMax buffer size: %d MB\n", max_buff_size);
 	printf("Total available memory : %d MB\n", dev_mem);
@@ -333,9 +327,8 @@ int Viterbi::viterbiLineOpenCL_cols(unsigned int *line_x, int g_low, int g_high)
 	err = clEnqueueWriteBuffer(m_command_queue, cmImg, CL_FALSE, 0, sizeof(unsigned char) * img_size, m_img, 0, NULL, NULL);
 
 	cl_mem cmLine_x = clCreateBuffer(m_context, CL_MEM_READ_WRITE, m_img_width * sizeof(int), NULL, &err);
-	//cl_mem cmX_cord = clCreateBuffer(m_context, CL_MEM_READ_WRITE, m_img_width * sizeof(int), NULL, &err);
-	cl_mem cmV1 = clCreateBuffer(m_context, CL_MEM_READ_WRITE, m_img_height * m_img_width * sizeof(float), NULL, &err);
-	cl_mem cmV2 = clCreateBuffer(m_context, CL_MEM_READ_WRITE, m_img_height * m_img_width * sizeof(float), NULL, &err);
+	cl_mem cmV1 = clCreateBuffer(m_context, CL_MEM_READ_WRITE, m_img_height * global_size * sizeof(float), NULL, &err);
+	cl_mem cmV2 = clCreateBuffer(m_context, CL_MEM_READ_WRITE, m_img_height * global_size * sizeof(float), NULL, &err);
 	cl_mem cmL = clCreateBuffer(m_context, CL_MEM_READ_WRITE, img_size * global_size * sizeof(float), NULL, &err);
 
 	//set kernel arguments
@@ -344,7 +337,6 @@ int Viterbi::viterbiLineOpenCL_cols(unsigned int *line_x, int g_low, int g_high)
 	err |= clSetKernelArg(m_viterbiKernel, 2, sizeof(cl_mem), (void*)&cmLine_x);
 	err |= clSetKernelArg(m_viterbiKernel, 3, sizeof(cl_mem), (void*)&cmV1);
 	err |= clSetKernelArg(m_viterbiKernel, 4, sizeof(cl_mem), (void*)&cmV2);
-	//err |= clSetKernelArg(m_viterbiKernel, 5, sizeof(cl_mem), (void*)&cmX_cord);
 	err |= clSetKernelArg(m_viterbiKernel, 5, sizeof(cl_int), (void*)&m_img_height);
 	err |= clSetKernelArg(m_viterbiKernel, 6, sizeof(cl_int), (void*)&m_img_width);
 	err |= clSetKernelArg(m_viterbiKernel, 7, sizeof(cl_int), (void*)&g_high);
@@ -354,27 +346,28 @@ int Viterbi::viterbiLineOpenCL_cols(unsigned int *line_x, int g_low, int g_high)
 	{
 		return err; //
 	}
+
 	//to big buffer will fail with CL_MEM_OBJECT_ALLOCATION_FAILURE - have to process it with chunks
 	//not all columns at the same time, call it couple of times
-	size_t start_column = 0; // each iteration add global_size untile start_column >= img_width
+	size_t first_col = 0; 
 	err = clEnqueueWriteBuffer(m_command_queue, cmLine_x, CL_FALSE, 0, sizeof(int) * m_img_width, line_x, 0, NULL, NULL);
-	while (start_column < m_img_width && !err)
+	while (first_col < m_img_width && !err)
 	{
-		err = clSetKernelArg(m_viterbiKernel, 9, sizeof(cl_int), (void*)&start_column);
+		err = clSetKernelArg(m_viterbiKernel, 9, sizeof(cl_int), (void*)&first_col);
 		err |= clEnqueueNDRangeKernel(m_command_queue, m_viterbiKernel, 1, NULL, &global_size, NULL, 0, NULL, NULL);
 
-		// Copy results from the memory buffer */
+		// Copy results from the memory buffer 
 		err |= clEnqueueReadBuffer(m_command_queue, cmLine_x, CL_TRUE, 0,
 			m_img_width * sizeof(int), line_x, 0, NULL, NULL);
-		start_column += global_size;
+		first_col += global_size;
 	}
+	
 	line_x[m_img_width - 1] = line_x[m_img_width - 2];
+	
 	//realase resources
-
 	err = clReleaseMemObject(cmLine_x);
 	err = clReleaseMemObject(cmL);
 	err = clReleaseMemObject(cmImg);
-//	err = clReleaseMemObject(cmX_cord);
 	err = clReleaseMemObject(cmV1);
 	err = clReleaseMemObject(cmV2);
 	return CL_SUCCESS;
@@ -434,7 +427,6 @@ unsigned int Viterbi::viterbiMultiThread(int g_low, int g_high, unsigned int sta
 	std::vector<int> L(m_img_height * m_img_width, 0);
 	std::vector<uint32_t> V(m_img_height * m_img_width, 0);
 
-	std::vector<uint32_t> x_cord(m_img_width, 0);
 	uint32_t max_val = 0;
 	unsigned char pixel_value = 0;
 	// init first column with zeros
@@ -481,11 +473,58 @@ unsigned int Viterbi::viterbiMultiThread(int g_low, int g_high, unsigned int sta
 		}
 	}
 	//backwards phase - retrace the path
-	x_cord[(m_img_width - 1)] = x_max;
+	uint32_t x_n = x_max;
 	for (size_t n = (m_img_width - 1); n > start_col; n--)
 	{
-		x_cord[n - 1] = x_cord[n] + L[(x_cord[n] * m_img_width) + (n - 1)];
+		x_n = x_n + L[(x_n * m_img_width) + (n - 1)];
 	}
-	// save only last pixel position
-	return static_cast<unsigned int>(x_cord[start_col]);
+	// save last pixel position
+	return static_cast<unsigned int>(x_n);
 }
+
+double Viterbi::viterbiHybridCPU(std::vector<unsigned int> line_x, int g_lo, int g_high, uint32_t start_col, uint32_t end_col)
+{
+	return 0;
+}
+
+double Viterbi::viterbiHybridGPU(std::vector<unsigned int> line_x, int g_lo, int g_high, uint32_t start_col, uint32_t end_col)
+{
+	return 0;
+}
+
+
+int Viterbi::launchHybridViterbi(std::vector<unsigned int>& line_x, int g_low, int g_high)
+{
+	uint32_t start_col_CPU = 0, end_col_CPU = static_cast<uint32_t>(m_hybrid_rate.first * static_cast<double>(m_img_width));
+	uint32_t start_col_GPU = end_col_CPU + 1, end_col_GPU = static_cast<uint32_t>(m_hybrid_rate.second * static_cast<double>(m_img_width));
+	uint8_t num_of_threads = std::thread::hardware_concurrency();
+	std::vector<unsigned int> line(num_of_threads);
+	std::vector<std::future<unsigned int> > viterbiThreads(num_of_threads);
+
+	std::future<double> cpu_thread = std::async(launch::async,
+		&Viterbi::viterbiHybridCPU, this, std::ref(line_x), g_low, g_high, start_col_CPU, end_col_CPU);
+	std::future<double> gpu_thread = std::async(launch::async,
+		&Viterbi::viterbiHybridGPU, this, std::ref(line_x), g_low, g_high, start_col_GPU, end_col_GPU);
+
+	double time_cpu = cpu_thread.get();
+	double time_gpu = gpu_thread.get();
+
+	line_x[m_img_width - 1] = line_x[m_img_width - 2];
+	bool success = false;
+	if (time_cpu > 0 && time_gpu > 0)
+	{
+		//calculate new rate
+		if (!m_set_hybrid_rate)
+		{
+			double rate = static_cast<double>(time_cpu);
+			m_hybrid_rate;// std::pair<double, double>(0.5, 0.5)
+		}
+		else
+		{
+			//handle check if image changed significantly its size in each dimension
+			//if not use old rate, else set default
+		}
+	}
+	return success;
+}
+
